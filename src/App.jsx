c5bf77757a23a86f36cf246f75014eee0754e73e
KWDev <kleberwfo51@gmail.com>
2026-07-29 23:33:21 -0300

    feat: cria componente principal da aplicacao

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Contador from './components/Contador.jsx'
+
+// Componente principal: apenas monta a tela e exibe o Contador
+function App() {
+  return (
+    <main>
+      <Contador />
+    </main>
+  )
+}
+
+export default App
